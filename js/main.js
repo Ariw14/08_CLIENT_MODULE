11c642dee0fcd440cc6129489f00e6f5b612313e
Ariw14 <ibuibu140208@gmail.com>
2025-05-26 11:41:13 +0800

    start game

--- js/main.js
+++ js/main.js
@@ -12,47 +12,40 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const usernameinput = document.getElementById('username');
-//     const levelSelect = document.getElementById('level');
-//     const btnstart = document.getElementById('btnstart');
-//     const countdown = document.getElementById('countdownstart');
-
-//     function validate() {
-//         const username = usernameinput.value.trim();
-//         const level = levelSelect.value;
-
-//         localStorage.getItem('username');
-
-//         const isValid = !username && !level;
-//         btnstart.disabled = !isValid;
-//     };
-
-//     usernameinput.addEventListener('input', validate);
-//     levelSelect.addEventListener('change', validate);
-
-//     btnstart.addEventListener('click', () => {
-//         const username = usernameinput.value.trim();
-//         const level = levelSelect.value;
-        
-//         const countdown = () => {
-//             let count = 3;
-//             while (count > 0) {
-//                 setInterval(countdown(count), 1000);
-//             count--;
-//             }
-//         }
-        
-//     });
-
-//     validate();
-// });
-
 document.addEventListener('DOMContentLoaded', () => {
-    const btnstart = document.getElementById('startbtn');
-    const contentgame = document.getElementById('content-game');
+    const usernameinput = document.getElementById('username');
+    const levelSelect = document.getElementById('level');
+    const btnstart = document.getElementById('btnstart');
+    const countdown = document.getElementById('countdownstart');
+    const contentgame = document.getElementById('contentgame');
 
-    btnstart.addEventListener('W', () => {
+    function validate() {
+        const username = usernameinput.value.trim();
+        const level = levelSelect.value;
+
+        const isValid = username !== '' && level !== '';
+        btnstart.disabled = !isValid;
+    };
+
+    usernameinput.addEventListener('input', validate);
+    levelSelect.addEventListener('change', validate);
+
+    btnstart.addEventListener('click', () => {
+        localStorage.setItem('username', usernameinput.value.trim());
         contentgame.classList.remove('hidden');
+        document.getElementById('welcome-page').classList.add('hidden');
+
+        const countdown = () => {
+            let count = 3;
+            while (count > 0) {
+                setInterval(countdown(count), 1000);
+            count--;
+            }
+        }
+        
     });
-})
+
+    validate();
+});
+
+
